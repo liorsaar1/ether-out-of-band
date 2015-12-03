9b901789a3b359f8ec5a09394ebab2da2c08883f
liorsaar1 <liorsaar1@gmail.com>
2015-12-03 15:31:37 +0000

    common test

--- app/js/webserver.js
+++ app/js/webserver.js
@@ -150,7 +150,9 @@ function response500(response, error) {
 // web3
 //=======================================
 var Web3 = require('web3');
+console.log(">>>>>>>>>>>>>>>>>>", Web3);
 var web3 = new Web3();
+console.log(">>>>>>>>>>>>>>>>>>????? ", web3);
 var providerUrl = 'http://lior.ide.tmp.ether.camp:8555/sandbox/7e967b0ebda3f76fb82c842fedbffb289a0ae474'
 web3.setProvider(new web3.providers.HttpProvider(providerUrl));
 web3.eth.defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
