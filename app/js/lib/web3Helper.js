9a7af478387509f1da01b455097145b0dc98e185
liorsaar1 <liorsaar1@gmail.com>
2015-12-03 21:57:25 +0000

    web3 helper

--- app/js/lib/web3Helper.js
+++ app/js/lib/web3Helper.js
@@ -1,13 +1,37 @@
-function getWeb3() {
-    var Web3 = require('web3');
-    var web3 = new Web3();
-    var providerUrl = 'http://out-of-band.on.ether.camp:8555/sandbox/7e967b0ebda3f76fb82c842fedbffb289a0ae474'
-    web3.setProvider(new web3.providers.HttpProvider(providerUrl));
-    web3.eth.defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
-    console.log( "web3.js host:", web3.currentProvider.host);
-    return web3;
+function getExport() {
+    return function() { // constructor
+        return {
+            getWeb3: function() {
+                var Web3 = require('web3');
+                var web3 = new Web3();
+                var providerUrl = 'http://out-of-band.on.ether.camp:8555/sandbox/7e967b0ebda3f76fb82c842fedbffb289a0ae474'
+                web3.setProvider(new web3.providers.HttpProvider(providerUrl));
+                web3.eth.defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
+                console.log( "web3.js host:", web3.currentProvider.host);
+                return web3;
+            }
+        }
+    }
 }
 
-function test() {
-    
+// shared code (do not remove !!!)
+if (typeof module === 'undefined') {
+    // html javascript inclusion
+    this['Web3Helper'] = getExport();
+} else {
+    // node js
+    module.exports = getExport();
 }
+
+
+
+
+// function getWeb3() {
+//     var Web3 = require('web3');
+//     var web3 = new Web3();
+//     var providerUrl = 'http://out-of-band.on.ether.camp:8555/sandbox/7e967b0ebda3f76fb82c842fedbffb289a0ae474'
+//     web3.setProvider(new web3.providers.HttpProvider(providerUrl));
+//     web3.eth.defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
+//     console.log( "web3.js host:", web3.currentProvider.host);
+//     return web3;
+// }
