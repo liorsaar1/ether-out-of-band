55ea2ccbc9bf095612398592e64591ecb1f48e19
liorsaar1 <liorsaar1@gmail.com>
2015-12-03 22:31:24 +0000

    web3 helper

--- app/js/webserver.js
+++ app/js/webserver.js
@@ -149,13 +149,8 @@ function response500(response, error) {
 //=======================================
 // web3
 //=======================================
-var Web3 = require('web3');
-console.log(">>>>>>>>>>>>>>>>>>", Web3);
-var web3 = new Web3();
-console.log(">>>>>>>>>>>>>>>>>>????? ", web3);
-var providerUrl = 'http://lior.ide.tmp.ether.camp:8555/sandbox/7e967b0ebda3f76fb82c842fedbffb289a0ae474'
-web3.setProvider(new web3.providers.HttpProvider(providerUrl));
-web3.eth.defaultAccount = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
+var web3Helper = require('./lib/web3Helper.js')();
+var web3 = web3Helper.getWeb3();
 
 //=======================================
 // namereg
