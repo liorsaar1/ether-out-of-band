7406bee3af7f45bea687bf4f64936db342816648
liorsaar1 <liorsaar1@gmail.com>
2015-12-05 03:50:40 +0000

    is known oracle

--- dapp/contracts/OutOfBand.sol
+++ dapp/contracts/OutOfBand.sol
@@ -1,5 +1,7 @@
 import "std.sol";
 
+/// sample oracle contract 
+/// interfaces between a meat world oracle server and a wallet contract
 contract Oracle is named("Oracle") {
     event Notify(string phone, uint value);
     
@@ -17,6 +19,7 @@ contract Oracle is named("Oracle") {
         Notify(phone, value);
     }
     
+    /// spend confirmed by the user via SMS
     function confirmed(string phone, uint value) {
         log0("Oracle: confirmed");
         // if (wallet == 0) {
@@ -25,6 +28,7 @@ contract Oracle is named("Oracle") {
         wallet.spendConfirmed(value);
     }
     
+    /// spend declined by the user via SMS
     function lock(string phone) {
         log0("Oracle: denied - lock");
         // if (wallet == 0) {
@@ -33,6 +37,16 @@ contract Oracle is named("Oracle") {
         wallet.lock();
     }
 }
+
+
+/// sample wallet, controlled from a web UI
+/// when a spend() call is made, the oracle is asked to send the user an out-of-band notification (SMS) in the meat world
+/// valid replies:
+///   confirm - execute the spend
+///   lock - the wallet is locked
+/// 
+/// note: the phone # and oracle(s) should be set upon construction and remain immutable
+/// for the purpose of this demo they are variable
 
 contract Wallet is named("Wallet") {
 
@@ -70,13 +84,32 @@ contract Wallet is named("Wallet") {
     
     function spendConfirmed(uint value) {
         log1("spendConfirmed", bytes32(value));
+        // only a known oracle can confirm
+        if (!isKnownOracle(msg.sender)) {
+            Feedback( "Unknown sender. Spend Confirmation message ignored."); // scary
+            return;
+        }
+        // spend
         Feedback( "Spend confirmed");
         spendAddress.send(spendAmount);
     }
     
     function lock() {
         log0("Spend Denied - Lock");
+        // only a known oracle can confirm
+        if (!isKnownOracle(msg.sender)) {
+            Feedback( "Unknown sender. Lock message ignored."); // scary
+            return;
+        }
+        // lock it
         Feedback( "Declined - Wallet locked");
         locked = true;
+    }
+    
+    function isKnownOracle(address msgSender) returns (bool){
+        if (oracle == msgSender ) {
+            return true;
+        }
+        return false;
     }
 }
